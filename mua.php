89ca6e224cb2a80027f52cadc8666d61b82bebef
hanschrs <hans_christian@ymail.com>
2017-09-15 15:04:41 +0700

    150917 1504

--- mua.php
+++ mua.php
@@ -55,22 +55,15 @@
     </div>
     <div class="row list-content">
       <div class="col-xs-10 col-xs-offset-1 panel-group portofolio">
-      <h2 class="text-center">Portofolio</h2>
+        <h2 class="text-center">Portofolio</h2>
         <div class="panel-sub">
-          <a href="mua.php" class="panel porto">
-            <img src="img/user_profile_female.jpg" alt="" class="img-thumbnail">
-            <!-- <div class="col-xs-12">
-              <p class="nama text-center">Irene Paulina</p>
-              <p class="harga text-center">Rp 300.000,-</p>
-            </div> -->
-          </a>
-          <a href="mua.php" class="panel porto">
-            <img src="img/user_profile_female.jpg" alt="" class="img-thumbnail">
-            <!-- <div class="col-xs-12">
-              <p class="nama text-center">Regina Martha</p>
-              <p class="harga text-center">Rp 400.000,-</p>
-            </div> -->
-          </a>
+          <ul class="list-inline panel-sub">
+            <li data-toggle="modal" data-target="#myModal" class="porto"><a href="#myGallery" data-slide-to="0"><img class="img-thumbnail" src="talents/MUA/mua_alexismua/1.jpg"></a></li>
+            <li data-toggle="modal" data-target="#myModal" class="porto"><a href="#myGallery" data-slide-to="1"><img class="img-thumbnail" src="talents/MUA/mua_alexismua/2.jpg"></a></li>
+            <li data-toggle="modal" data-target="#myModal" class="porto"><a href="#myGallery" data-slide-to="2"><img class="img-thumbnail" src="talents/MUA/mua_alexismua/3.jpg"></a></li>
+            <li data-toggle="modal" data-target="#myModal" class="porto"><a href="#myGallery" data-slide-to="3"><img class="img-thumbnail" src="talents/MUA/mua_alexismua/4.jpg"></a></li>
+            <!--end of thumbnails-->
+          </ul>
         </div>
         <div class="panel-sub">
           <a href="mua.php" class="panel porto">
@@ -94,18 +87,52 @@
       <p>MUA or Photographer?</p>
       <div class="joinButton"><button class="btn btn-lg">Join Us!</button></div>
     </div>
-  </div>
 
-  <script>
-    $(document).ready(function() {   
-      var sideslider = $('[data-toggle=collapse-side]');
-      var sel = sideslider.attr('data-target');
-      var sel2 = sideslider.attr('data-target-2');
-      sideslider.click(function(event){
-        $(sel).toggleClass('in');
-        $(sel2).toggleClass('out');
-      });
-    });
-  </script>
-</body>
-</html>
+
+    <!--begin modal window-->
+    <div class="modal fade" id="myModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <div class="pull-left">My Gallery Title</div>
+            <button type="button" class="close" data-dismiss="modal" title="Close"> <span class="glyphicon glyphicon-remove"></span></button>
+          </div>
+          <div class="modal-body">
+
+            <!--CAROUSEL CODE GOES HERE-->
+            <!--begin carousel-->
+            <div id="myGallery" class="carousel slide" data-interval="false">
+              <div class="carousel-inner">
+                <div class="item active"> <img src="talents/MUA/mua_alexismua/1.jpg" alt="item0"></div>
+                <div class="item"> <img src="talents/MUA/mua_alexismua/2.jpg" alt="item1"></div>
+                <div class="item"> <img src="talents/MUA/mua_alexismua/3.jpg" alt="item2"></div>
+                <div class="item"> <img src="talents/MUA/mua_alexismua/4.jpg" alt="item3"></div>
+                <!--end carousel-inner--></div>
+                <!--Begin Previous and Next buttons-->
+                <a class="left carousel-control" href="#myGallery" role="button" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#myGallery" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <!--end carousel--></div>
+                <!--end modal-body--></div>
+                <div class="modal-footer">
+                  <div class="pull-left">
+                    <small>Photographs by <a href="https://placeimg.com" target="new">placeimg.com</a></small>
+                  </div>
+                  <button class="btn-sm close" type="button" data-dismiss="modal">Close</button>
+                  <!--end modal-footer--></div>
+                  <!--end modal-content--></div>
+                  <!--end modal-dialoge--></div>
+                  <!--end myModal-->></div>
+                </div>
+
+                <script>
+                  $(document).ready(function() {   
+                    var sideslider = $('[data-toggle=collapse-side]');
+                    var sel = sideslider.attr('data-target');
+                    var sel2 = sideslider.attr('data-target-2');
+                    sideslider.click(function(event){
+                      $(sel).toggleClass('in');
+                      $(sel2).toggleClass('out');
+                    });
+                  });
+                </script>
+              </body>
+              </html>
